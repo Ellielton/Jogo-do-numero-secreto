c2a01a33f076f4d2d884fec9891dab5072e6fee5
Ellielton <ellieltongama@gmail.com>
2024-08-17 23:45:18 -0300

    Manipulando os valores na tela e na funçao com referências

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,6 +1,17 @@
+//Manipula os valores na função gerarAleatoria e na tela
+const menorVAlor = 5
+const maiorValor = 100
 const nuemroAleatorio = gerarNumeroAleatorio()
 
 function gerarNumeroAleatorio() {
-  return parseInt(Math.random() * 100)
+  //Arendonda random pra cima, multipraco por maiorValor que inclusivo com +1
+  return parseInt(Math.random() * maiorValor + 1)
 }
 console.log(`O número secreto é ${nuemroAleatorio}`)
+
+//Captura e atualizar a faixa de valores na tela.
+const elementoMenorValor = document.getElementById('menor-valor')
+elementoMenorValor.innerHTML= menorVAlor;
+
+const elementoMaiorValor = document.getElementById('maior-valor')
+elementoMaiorValor.innerHTML= maiorValor;
