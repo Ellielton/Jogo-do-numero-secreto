09b28f447c8a8e7873c0c16533ba122978abca55
Ellielton <ellieltongama@gmail.com>
2024-08-18 01:08:16 -0300

    Capturando o oudio do microfine do dispositvo e exibindo excrito no console do nevegador

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,5 +1,12 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
 const recognition = new SpeechRecognition();
 recognition.lang = 'pt-Br'
 recognition.start()
+
+//Esse escutador captura o que foi falado quando iniciar a aplicação e por meio de callback exibe escritp no console
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+  console.log(e.results[0][0].transcript)
+}
